032a6b32f03e17564504f5c35e07a1e68987415c
hyewon <gpdnjs154@naver.com>
2024-05-01 02:39:13 +0900

    [update]page에 추가

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,18 @@ import Image from "next/image";
 import Products from "./components/product";
 import Address from "./components/address";
 import { Form } from "@/components/ui/form";
+import Customer from "./components/customer";
+import Coupons from "./components/coupons";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 mb-[300px]">
+      <div className="flex flex-col gap-[15px]">
         <Products />
+        <Customer />
+
         <Address />
+        <Coupons />
       </div>
     </main>
   );
